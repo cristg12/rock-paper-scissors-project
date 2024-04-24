1f97a345b1ff132d604cd7cd3d322bda6236c715
Cristian <cristiantrenado41@gmail.com>
2024-04-24 22:56:48 +0200

    solution

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
-function getComputerChoice  () {
-    const option = ["rock", "paper", "scissors"]
-    return  option[Math.floor(Math.random() * option.length)]
-    
- };
- let computerSelection = getComputerChoice();
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+const resultElement = document.getElementById("results");
+const scoreElement = document.querySelector("#score");
 
-const playerSelection = "rock";
+let playerWins = 0;
+let computerWins = 0;
 
-function playRound (playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        return "You tie!";
-
-    } else if (playerSelection !== computerSelection && computerSelection === "paper"){
-        return "You loose!";
-    } else {
-        return "You win!";
-  
-    }
-};
-
-
-function playGame () {
-    let result = 0;
-    let playerWins = 0;
-    let computerWins = 0;
-
-    while (result < 5) {
-        computerSelection = getComputerChoice()
-        if(playRound (playerSelection, computerSelection) === "You win!"){
-            playerWins += 1;
-        } else if (playRound(playerSelection, computerSelection) === "You loose!") {
-            computerWins += 1;
-        }
-        
-        result++;
-    }
-
-    if(playerWins >= 3 && computerWins <=2){
-        console.log("player wins!")
-        
-    } else if (computerWins >= 3 && playerWins <= 2) {
-        console.log ("computer wins")
-    }
-
-    console.log(playerWins);
-        console.log(computerWins)
-    return result;
-   
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-let result = playGame();
+function playRound(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
 
+function updateScore() {
+    scoreElement.innerHTML = "Score: " + playerWins + " " + computerWins;
+}
 
+function playGame() {
+    if (playerWins >= 3 || computerWins >= 3) {
+        if (playerWins >= 3) {
+            console.log("Player wins the game!");
+        } else {
+            console.log("Computer wins the game!");
+        }
+        return;
+    }
 
+    let computerSelection = getComputerChoice();
+    let roundResult = playRound(userChoice, computerSelection);
 
-console.log(playRound(playerSelection, computerSelection))
+    resultElement.innerHTML += "<br>" + roundResult; // Agregar el resultado de la ronda al historial de resultados
+
+    if (roundResult === "You win!") {
+        playerWins++;
+    } else if (roundResult === "You lose!") {
+        computerWins++;
+    }
+
+    updateScore();
+}
+
+btnRock.addEventListener("click", () => {
+    userChoice = "rock";
+    playGame();
+});
+
+btnPaper.addEventListener("click", () => {
+    userChoice = "paper";
+    playGame();
+});
+
+btnScissors.addEventListener("click", () => {
+    userChoice = "scissors";
+    playGame();
+});
